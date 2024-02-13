134bb13f8f57ddfa8b6e68664a518307a0124f61
symoon007 <symoonma@gmail.com>
2024-02-13 13:38:58 +0600

    fix: rating issue

--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -8,7 +8,8 @@ import {
   FaShareAlt,
   FaStar,
 } from "react-icons/fa";
-import Rating from "react-rating";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 import { Link } from "react-router-dom";
 
 const NewsCard = ({ news }) => {
@@ -44,16 +45,9 @@ const NewsCard = ({ news }) => {
         </Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted d-flex">
-        <div className="flex-grow-1">
-          <Rating
-            className="me-1"
-            readonly
-            placeholderRating={rating?.number}
-            emptySymbol={<FaRegStar />}
-            placeholderSymbol={<FaStar className="text-warning" />}
-            fullSymbol={<FaStar />}
-          />
-          <span>{rating?.number}</span>
+        <div className="flex-grow-1 d-flex align-items-center">
+          <Rating style={{ maxWidth: 150 }} value={rating.number} readOnly />
+          <span className="ms-2">{rating?.number}</span>
         </div>
         <div>
           <FaEye /> {total_view}
